d3a275f1c4daf12acc38dd4f0a78257d54915a51
Joe Olson <jolson7168@nododos.com>
2017-02-27 07:20:30 -0600

    initial add

---
+++ code/queries/testQuery01/src/main/java/com/ocient/netflowdriver/queries/NetFlowArgs.java
@@ -0,0 +1,7 @@
+package com.ocient.netflowdriver.queries;
+
+/**
+ * Created by jjo31420 on 2/25/17.
+ */
+public class NetFlowArgs {
+}
---
+++ code/queries/testQuery01/src/main/scala/com/ocient/netflowdriver/queries/QueryTest01.scala
@@ -0,0 +1,8 @@
+package com.ocient.netflowdriver.queries
+
+/**
+  * Created by jjo31420 on 2/25/17.
+  */
+object QueryTest01 {
+
+}
